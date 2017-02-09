a947c078a98df6e91a9e69ada3edd7f80c3e0580
theodinspire <bobryuu@gmail.com>
2017-02-08 21:00:27 -0600

    Still trying to get those Pod files in

---
+++ Pods/Target Support Files/AWSRekognition/AWSRekognition-umbrella.h
@@ -0,0 +1,12 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "AWSRekognition.h"
+#import "AWSRekognitionModel.h"
+#import "AWSRekognitionResources.h"
+#import "AWSRekognitionService.h"
+
+FOUNDATION_EXPORT double AWSRekognitionVersionNumber;
+FOUNDATION_EXPORT const unsigned char AWSRekognitionVersionString[];
+
---
+++ Pods/Target Support Files/Pods-FingerBlade/Pods-FingerBlade-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_FingerBladeVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_FingerBladeVersionString[];
+
